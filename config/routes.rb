f3d334c4f127e8896be06fbe0a67597541079964
hitosi0317 <kondahitosi@gmail.com>
2021-05-21 06:36:22 +0900

    カレンダー機能カレンダー表示、予定登録完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
   get 'user/index'
   devise_for :users
   root to: 'motions#index'
+  resources :meetings
   resources :motions, only: [:index, :new, :create, :edit, :update, :destroy]
 end
