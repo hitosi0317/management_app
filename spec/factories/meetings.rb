ef70c2ebaf2ad7ef0188cbf721a7072f84de4feb
hitosi0317 <kondahitosi@gmail.com>
2021-05-23 23:41:56 +0900

    rubocopの実行

--- spec/factories/meetings.rb
+++ spec/factories/meetings.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :meeting do
-    name { "MyString" }
-    start_time { "2021-05-21 03:06:35" }
+    name { 'MyString' }
+    start_time { '2021-05-21 03:06:35' }
   end
 end
