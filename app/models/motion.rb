e977d5e08cae31e39e314b6145ae8bba8fdae840
hitosi0317 <kondahitosi@gmail.com>
2021-05-29 03:15:57 +0900

    テーブルの値とモデルのバリデーション変更

--- app/models/motion.rb
+++ app/models/motion.rb
@@ -1,16 +1,15 @@
 class Motion < ApplicationRecord
   belongs_to :user
-  has_many :histories
 
-  validates :training1, presence: true
-  validates :count1, :weight, numericality: true
+  validates :plans1, presence: true
+  validates :weight, numericality: true
 
-  validates :count2, :count3, :count4, :count5, numericality: true, unless: :count?
+  validates :count2, :count3, :count4, :count5, presence: true, unless: :count?
 
   def count?
-    count2.blank?
-    count3.blank?
-    count4.blank?
-    count5.blank?
+    plans2.blank?
+    plans3.blank?
+    plans4.blank?
+    plans5.blank?
   end
 end
