1e3d893e2b2c5b77a84e35e566a4c43c8a00d074
hitosi0317 <kondahitosi@gmail.com>
2021-05-31 00:30:28 +0900

    javascipt予定完了機能完了

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'motions#index'
   resources :meetings
-  resources :motions, only: [:index, :new, :create, :edit, :update, :destroy, :show]
+  get 'motion/:id', to: 'motions#checked'
+  resources :motions
+
 end
